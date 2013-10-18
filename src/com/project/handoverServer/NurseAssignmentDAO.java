24a7a16b0c75f3944e4b96a3e653ddd755a94a07
Qi <wwqq03@gmail.com>
2013-10-18 20:40:30 +0200

    Add Class which edits the callplan file

---
+++ src/com/project/handoverServer/NurseAssignmentDAO.java
@@ -0,0 +1,78 @@
+package com.project.handoverServer;
+
+import java.util.Iterator;
+import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+
+import org.dom4j.io.*;
+import org.dom4j.*;
+
+public class NurseAssignmentDAO {
+	
+	private static final String CALLPLANFILE = "callplan.xml";
+	
+	private NurseAssignmentDAO() {};
+	
+	public static synchronized boolean editCallPlan(String room, String nurse){
+		if(room == null || nurse == null)
+			return false;
+		try{
+			SAXReader reader = new SAXReader();
+			Document  document = reader.read(new File(CALLPLANFILE));
+			
+			Element callplan = document.getRootElement();
+			List<Element> roomList = callplan.elements("room");
+			
+			for(Iterator<Element> itRoom = roomList.iterator(); itRoom.hasNext();){
+				Element roomElement = itRoom.next();
+				Attribute uriAttribute = roomElement.attribute("uri");
+				if(uriAttribute == null)
+					continue;
+				
+				String uriString = uriAttribute.getText();
+				if(!room.equals(getNameFromUri(uriString)))
+					continue;
+				
+				List<Element> nurseList = roomElement.elements("nurse");
+				for(Iterator<Element> itNurse = nurseList.iterator(); itNurse.hasNext();){
+					Element nurseElement = itNurse.next();
+					Attribute rank = nurseElement.attribute("rank");
+					
+					if(rank == null)
+						continue;
+					if(!rank.getText().equals("1"))
+						continue;
+					
+					String nurseUri = nurseElement.getText();
+					String[] temp = nurseUri.split("@");
+					if(temp.length < 2)
+						return false;
+					nurseElement.setText(nurse + "@" + temp[1]);
+					
+					XMLWriter writer = new XMLWriter(new FileWriter(CALLPLANFILE));
+					writer.write(document);
+					writer.close();
+					
+					return true;
+				}
+			}
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+	
+	private static String getNameFromUri(String uri) {
+		if(uri == null)
+			return null;
+		if(!uri.contains("@"))
+			return null;
+		String[] names = uri.split("@");
+		return names[0];
+	}
+
+}
